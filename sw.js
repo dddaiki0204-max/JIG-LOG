c547da044a6395c8031e046133448bab7c4beeaf
dddaiki0204-max <dddaiki0204@gmail.com>
2026-09-15 01:04:40 +0900

    Add files via upload
    PWA対応

---
+++ sw.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = "jig-log-v1";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./catch.html",
+  "./catches.html",
+  "./fishing.html",
+  "./trips.html",
+  "./trip-detail.html",
+  "./jigs.html",
+  "./tackle.html",
+  "./analysis.html",
+  "./tide-settings.html",
+  "./style.css",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => {
+        if (cached) return cached;
+        if (event.request.mode === "navigate") return caches.match("./index.html");
+        return new Response("", { status: 503, statusText: "Offline" });
+      }))
+  );
+});
